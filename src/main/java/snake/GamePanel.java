c730b1c40b7193269ea8e44683e6d2998166b748
Karol Programuje <karol341654@gmail.com>
2023-10-01 10:25:20 +0200

    Initial commit

---
+++ src/main/java/snake/GamePanel.java
@@ -0,0 +1,17 @@
+package main.java.snake;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    public GamePanel() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
