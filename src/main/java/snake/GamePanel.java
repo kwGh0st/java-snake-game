4dc8b7b3b7b92507756772c89529a27e2d467d38
Karol Programuje <karol341654@gmail.com>
2023-10-01 10:33:53 +0200

    Declaring fields

--- src/main/java/snake/GamePanel.java
+++ src/main/java/snake/GamePanel.java
@@ -1,12 +1,58 @@
 package main.java.snake;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+    private static final int SCREEN_WIDTH = 600;
+    private static final int SCREEN_HEIGHT = 600;
+    private static final int UNIT_SIZE = 25;
+    private static final int GAME_UNITS = (SCREEN_WIDTH/SCREEN_HEIGHT)/UNIT_SIZE;
+    private static final int DELAY = 75;
+    private final int[] x = new int[GAME_UNITS];
+    private final int[] y = new int[GAME_UNITS];
+    private int bodyParts = 6;
+    private int applesEaten;
+    private int appleX;
+    private int appleY;
+    private char direction = 'R';
+    private boolean running = false;
+    private Timer timer;
+    private Random random;
+
     public GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
 
     }
 
